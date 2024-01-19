cf98aba0aa1aff910df3d5267729a2b31a1b650d
evaliu2002 <evaxinerliu@gmail.com>
2024-01-18 19:12:14 -0800

    asdfasdf

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './testing/reportWebVitals';
+import * as monaco from 'monaco-editor';
+import { customLanguageWorker } from './languages/antlr/CustomLanguageWorker';
+
+// Define the getWorker function
+// Dummy web worker function for now, will build more upon when parser and syntax highlighting is created
+const getWorker = (_moduleId: string, label: string): Worker => {
+  return new Worker(
+    URL.createObjectURL(
+      new Blob([`self.onmessage = ${customLanguageWorker}`], {
+        type: 'application/javascript',
+      })
+    )
+  );
+};
+
+// Set the getWorker function in MonacoEnvironment
+(window as any).MonacoEnvironment = {
+  getWorker: getWorker,
+};
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
 );
 
